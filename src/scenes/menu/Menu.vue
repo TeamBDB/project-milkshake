206cf3fdfa1487372e24fe96b744f30628c95163
Bruno Brito <brunodb3@gmail.com>
2022-07-31 01:45:57 +0200

    feat(ci/cd): adding Firebase hosting and deploy script

--- src/scenes/menu/Menu.vue
+++ src/scenes/menu/Menu.vue
@@ -1,5 +1,16 @@
 <template>
+  <div id="menu" class="fullscreen"></div>
   <div>
     <h5>Game Menu!</h5>
   </div>
 </template>
+
+<style scoped>
+#menu {
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center center;
+  background-image: url(/img/temp_city_background.png);
+}
+</style>
